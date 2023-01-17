c0ee7d91d5e3011c287bb8abe0118bdd48cd032c
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-17 12:35:50 -0600

    added array range

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,5 +1,18 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        int[] a = {(Integer.parseInt(args[0])), (Integer.parseInt(args[1])), (Integer.parseInt(args[2]))};
+        int maxValue = a[0];
+        int minValue = a[0];
+        for (int i = 0; i < a.length; i ++){
+            if (maxValue<a[i]){
+                maxValue = a[i];
+            }
+        }
+        for (int i = 0; i < a.length; i ++){
+            if (minValue>a[i]){
+                minValue = a[i];
+            }
+        }
+        System.out.println(maxValue-minValue);
     }
 }
