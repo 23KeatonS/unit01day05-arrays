b408087a567504bbbd9c1d67a307777f350dc26a
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-18 11:17:06 -0600

    added immutable sort

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,26 @@
+import java.util.Arrays;
 public class ImmutableSort {
+    
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        
+        int[] a = {(Integer.parseInt(args[0])), (Integer.parseInt(args[1])), (Integer.parseInt(args[2]))};
+        int maxValue = 0;
+        int minValue = 0;
+        for(int i = 0; i<a.length;i++){
+            if (a[maxValue]<a[i]){
+                maxValue = i;
+            }
+            if (a[minValue]>=a[i]){
+                minValue = i;
+            }
+        }
+        int[] numList = new int[3];
+        numList[0] = a[minValue];
+        numList[2] = a[maxValue];
+        numList[1] = a[3-maxValue-minValue];
+        
+        System.out.println(Arrays.toString(numList));
+
         
     }
 }
